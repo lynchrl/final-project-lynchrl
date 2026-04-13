5b82bf49973964e5a442aa23c71b25d79d035ae5
Ryan Lynch <rlynch5@gmail.com>
2026-04-13 10:11:31 -0600

    Add flag for local srcdir override

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ git submodule init
 git submodule sync
 git submodule update
 
+export BME280_OVERRIDE_SRCDIR=/home/lynchrl/src/MSCS/msee-embedded-linux/final-project-lynchrl-src
+
 set -e 
 cd `dirname $0`
 
@@ -28,6 +30,10 @@ then
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
+	# If first arg is "local", then set BME280_OVERRIDE_SRCDIR.
+	if [ "$1" = "local" ]; then
+		make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BME280_OVERRIDE_SRCDIR=${BME280_OVERRIDE_SRCDIR}
+	else
+		make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
+	fi
 fi
