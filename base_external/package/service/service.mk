c8074ac983beab20f88cadf9f876ac9d87c8a0c9
Ryan Lynch <rlynch5@gmail.com>
2026-04-21 09:41:16 -0600

    Update BME280 service configuration and increase root filesystem size

--- base_external/package/service/service.mk
+++ base_external/package/service/service.mk
@@ -1,4 +1,4 @@
-SERVICE_VERSION = '3e95659002956535e5dc8bfd0fb60450238642b1'
+SERVICE_VERSION = 'c37de7dd63cbdf68419b5e5ea5a4e2b46a8058fa'
 SERVICE_SITE = $(call github,lynchrl,final-project-lynchrl-src,$(SERVICE_VERSION))
 
 SERVICE_DEPENDENCIES = sqlite
@@ -6,5 +6,10 @@ SERVICE_DEPENDENCIES = sqlite
 SERVICE_SUBDIR = service
 SERVICE_GOMOD = github.com/lynchrl/final-project-lynchrl-src/service
 SERVICE_BUILD_TARGETS = .
+
+define SERVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/bin/service -t $(TARGET_DIR)/opt/bmeservice
+	$(INSTALL) -D -m 644 $(@D)/service/index.html -t $(TARGET_DIR)/opt/bmeservice
+endef
 
 $(eval $(golang-package))
